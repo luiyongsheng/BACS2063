2cbb17309f360c6a7b7c8957d409259a392f4f0a
Lui Yong Sheng <sheng_9707@live.com>
2018-07-09 08:33:40 +0800

    Create SetInterface.java

---
+++ SetInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical.pkg3;
+
+/**
+ *
+ * @author Sylvia Tan
+ */
+public interface SetInterface<T> {
+    
+    public boolean add(T newElement);
+    public boolean remove(T anElement);
+    public void union(SetInterface anotherSet);
+    public SetInterface intersection(SetInterface anotherSet);
+    public boolean checkSubset(SetInterface anotherSet);
+    public boolean isEmpty();
+}
